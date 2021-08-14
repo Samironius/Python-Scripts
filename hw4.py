677e88cb53fb4b39b02e853ce0f101b4529defe1
Samir <Samirshahub@gmail.com>
2021-08-15 01:56:57 +0300

    TEAM-10 feat: Create program that outputs header field rpm.RPMTAG_RELEASE of this file

--- hw4.py
+++ hw4.py
@@ -1,10 +1,7 @@
-import os
-import rpm
+import os, sys, rpm
 
-
-
-
-rpm_file = os.open("apacheds-2.0.0.AM26-x86_64.rpm", os.O_RDONLY)
+path = sys.argv[1]
+rpm_file = os.open("path", os.O_RDONLY)
 ts = rpm.TransactionSet()
 package = ts.hdrFromFdno(rpm_file)
-print("RELEASE...:", package[rpm.RPMTAG_RELEASE])
+print(package[rpm.RPMTAG_RELEASE])
