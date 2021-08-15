8ea4da260c46f8571e55cf9250442dff2a24a856
Samir <Samirshahub@gmail.com>
2021-08-15 15:10:28 +0300

    TEAM-10 feat: program that parses a JSON file

--- hw2.py
+++ hw2.py
@@ -13,3 +13,5 @@ result = {"id": data["matrix"][IHV]["id"], "number": data["matrix"][IHV]["number
           "committer_name": data["committer_name"], "committer_email": data["committer_email"], }
 
 json.dump(result, open(path_to_result, "w"))
+
+#coment for jiraaaa
