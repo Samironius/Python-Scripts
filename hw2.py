7e2597b4be7dc53be478afa9953548926e835f4b
Samir <Samirshahub@gmail.com>
2021-08-18 03:26:36 +0300

    TEAM-10 fix: non-zero 'result'

--- hw2.py
+++ hw2.py
@@ -3,15 +3,18 @@ import sys, json
 path_to_files, path_to_result = sys.argv[1], sys.argv[2]
 data = json.load(open(path_to_files, "r"))
 
-ss = []
+res_no_0 = []
+oll_res = []
 for i in data["matrix"]:
-    ss.append(i["number"])
-IHV = ss.index(max(ss))
-print(IHV)
+    oll_res.append(i["number"])
+    if i["result"] != 0:
+        res_no_0.append(i["number"])
+IHV = oll_res.index(max(res_no_0))
 
 result = {"id": data["matrix"][IHV]["id"], "number": data["matrix"][IHV]["number"],
           "committer_name": data["committer_name"], "committer_email": data["committer_email"], }
 
 json.dump(result, open(path_to_result, "w"))
+print(result)
 
-#coment for jiraaaa
+
